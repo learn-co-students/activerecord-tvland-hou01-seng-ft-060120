2ece26f523a32d24080858a17436ffd83aa40c99
Julio <julio.chazari@gmail.com>
2020-06-13 01:42:08 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+    
+    def full_name
+        return "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+        all_roles = []
+        self.characters.each do | character |
+            all_roles << "#{character.name} - #{character.show.name}"
+        end
+        all_roles
+    end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+    belongs_to :network
+    has_many :characters
+
+    def actors_list
+        actors_names = []
+        self.characters.each do | character |
+            first_name = character.actor.first_name
+            last_name = character.actor.last_name
+            actors_names << "#{first_name} #{last_name}"
+        end
+        actors_names
+    end
 end
